f8a0b909000ef1006c9c36ad49a00d5c7a10881f
james sa <james.sa.2013@gmail.com>
2015-06-18 13:56:57 +0800

    Fix format

--- Assets/Scripts/Stitcher.cs
+++ Assets/Scripts/Stitcher.cs
@@ -3,85 +3,83 @@ using System.Collections.Generic;
 
 public class Stitcher
 {
-    /// <summary>
-    /// Stitch clothing onto an avatar.  Both clothing and avatar must be instantiated however clothing may be destroyed after.
-    /// </summary>
-    /// <param name="sourceClothing"></param>
-    /// <param name="targetAvatar"></param>
-    /// <returns>Newly created clothing on avatar</returns>
-    public GameObject Stitch(GameObject sourceClothing, GameObject targetAvatar)
-    {
-        var boneCatelog = new TransformCatelog(targetAvatar.transform);
-        var skinnedMeshRenderers = sourceClothing.GetComponentsInChildren<SkinnedMeshRenderer>();
-        var targetClothing = AddChild(sourceClothing, targetAvatar.transform);
-        foreach (var sourceRenderer in skinnedMeshRenderers)
-        {
-            var targetRenderer = AddSkinnedMeshRenderer(sourceRenderer, targetClothing);
-            targetRenderer.bones = TranslateTransforms(sourceRenderer.bones, boneCatelog);
-        }
-        return targetClothing;
-    }
+	/// <summary>
+	/// Stitch clothing onto an avatar.  Both clothing and avatar must be instantiated however clothing may be destroyed after.
+	/// </summary>
+	/// <param name="sourceClothing"></param>
+	/// <param name="targetAvatar"></param>
+	/// <returns>Newly created clothing on avatar</returns>
+	public GameObject Stitch (GameObject sourceClothing, GameObject targetAvatar)
+	{
+		var boneCatelog = new TransformCatelog (targetAvatar.transform);
+		var skinnedMeshRenderers = sourceClothing.GetComponentsInChildren<SkinnedMeshRenderer> ();
+		var targetClothing = AddChild (sourceClothing, targetAvatar.transform);
+		foreach (var sourceRenderer in skinnedMeshRenderers) {
+			var targetRenderer = AddSkinnedMeshRenderer (sourceRenderer, targetClothing);
+			targetRenderer.bones = TranslateTransforms (sourceRenderer.bones, boneCatelog);
+		}
+		return targetClothing;
+	}
 
+	private GameObject AddChild (GameObject source, Transform parent)
+	{
+		var target = new GameObject (source.name);
+		target.transform.parent = parent;
+		target.transform.localPosition = source.transform.localPosition;
+		target.transform.localRotation = source.transform.localRotation;
+		target.transform.localScale = source.transform.localScale;
+		return target;
+	}
 
-    private GameObject AddChild(GameObject source, Transform parent)
-    {
-        var target = new GameObject(source.name);
-        target.transform.parent = parent;
-        target.transform.localPosition = source.transform.localPosition;
-        target.transform.localRotation = source.transform.localRotation;
-        target.transform.localScale = source.transform.localScale;
-        return target;
-    }
+	private SkinnedMeshRenderer AddSkinnedMeshRenderer (SkinnedMeshRenderer source, GameObject parent)
+	{
+		var target = parent.AddComponent<SkinnedMeshRenderer> ();
+		target.sharedMesh = source.sharedMesh;
+		target.materials = source.materials;
+		return target;
+	}
 
-    private SkinnedMeshRenderer AddSkinnedMeshRenderer(SkinnedMeshRenderer source, GameObject parent)
-    {
-        var target = parent.AddComponent<SkinnedMeshRenderer>();
-        target.sharedMesh = source.sharedMesh;
-        target.materials = source.materials;
-        return target;
-    }
-
-    private Transform[] TranslateTransforms(Transform[] sources, TransformCatelog transformCatelog)
-    {
-        var targets = new Transform[sources.Length];
-        for (var index = 0; index < sources.Length; index++)
-            targets[index] = DictionaryExtensions.Find(transformCatelog, sources[index].name);
-        return targets;
-    }
+	private Transform[] TranslateTransforms (Transform[] sources, TransformCatelog transformCatelog)
+	{
+		var targets = new Transform[sources.Length];
+		for (var index = 0; index < sources.Length; index++)
+			targets [index] = DictionaryExtensions.Find (transformCatelog, sources [index].name);
+		return targets;
+	}
 
 
     #region TransformCatelog
-    private class TransformCatelog : Dictionary<string, Transform>
-    {
+	private class TransformCatelog : Dictionary<string, Transform>
+	{
         #region Constructors
-        public TransformCatelog(Transform transform)
-        {
-            Catelog(transform);
-        }
+		public TransformCatelog (Transform transform)
+		{
+			Catelog (transform);
+		}
         #endregion
 
         #region Catelog
-        private void Catelog(Transform transform)
-        {
-            Add(transform.name, transform);
-            foreach (Transform child in transform)
-                Catelog(child);
-        }
+		private void Catelog (Transform transform)
+		{
+			Add (transform.name, transform);
+			foreach (Transform child in transform)
+				Catelog (child);
+		}
         #endregion
-    }
+	}
     #endregion
 
 
     #region DictionaryExtensions
-    private class DictionaryExtensions
-    {
-        public static TValue Find<TKey, TValue>(Dictionary<TKey, TValue> source, TKey key)
-        {
-            TValue value;
-            source.TryGetValue(key, out value);
-            return value;
-        }
-    }
+	private class DictionaryExtensions
+	{
+		public static TValue Find<TKey, TValue> (Dictionary<TKey, TValue> source, TKey key)
+		{
+			TValue value;
+			source.TryGetValue (key, out value);
+			return value;
+		}
+	}
     #endregion
 
 }
--- Assets/Scripts/Tester.cs
+++ Assets/Scripts/Tester.cs
@@ -4,45 +4,46 @@ using System.Collections.Generic;
 public class Tester : MonoBehaviour
 {
     #region Fields
-    public List<GameObject> clothing;
-    public GameObject avatar;
-    private GameObject wornClothing;
-    const int buttonWidth = 200;
-    const int buttonHeight = 20;
-    private Stitcher stitcher;
+	public List<GameObject> clothing;
+	public GameObject avatar;
+	private GameObject wornClothing;
+	const int buttonWidth = 200;
+	const int buttonHeight = 20;
+	private Stitcher stitcher;
     #endregion
 
     #region Monobehaviour
-    public void Awake()
-    {
-        stitcher = new Stitcher();
-    }
-    public void OnGUI()
-    {
-        var offset = 0;
-        foreach (var cloth in clothing)
-        {
-            if (GUI.Button(new Rect(0, offset, buttonWidth, buttonHeight), cloth.name))
-            {
-                RemoveWorn();
-                Wear(cloth);
-            }
-            offset += buttonHeight;
-        }
-    }
+	public void Awake ()
+	{
+		stitcher = new Stitcher ();
+	}
+
+	public void OnGUI ()
+	{
+		var offset = 0;
+		foreach (var cloth in clothing) {
+			if (GUI.Button (new Rect (0, offset, buttonWidth, buttonHeight), cloth.name)) {
+				RemoveWorn ();
+				Wear (cloth);
+			}
+			offset += buttonHeight;
+		}
+	}
     #endregion
 
-    private void RemoveWorn()
-    {
-        if (wornClothing == null) return;
-        GameObject.Destroy(wornClothing);
-    }
+	private void RemoveWorn ()
+	{
+		if (wornClothing == null)
+			return;
+		GameObject.Destroy (wornClothing);
+	}
 
-    private void Wear(GameObject clothing)
-    {
-        if (clothing == null) return;
-        clothing = (GameObject)GameObject.Instantiate(clothing);
-        wornClothing = stitcher.Stitch(clothing, avatar);
-        GameObject.Destroy(clothing);
-    }
+	private void Wear (GameObject clothing)
+	{
+		if (clothing == null)
+			return;
+		clothing = (GameObject)GameObject.Instantiate (clothing);
+		wornClothing = stitcher.Stitch (clothing, avatar);
+		GameObject.Destroy (clothing);
+	}
 }
